bc6dafaa9e6040971a3c9ef998a73c24c10bb7e5
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-24 17:20:08 +0200

    added internal card

--- packages/internal-project/src/app.tsx
+++ packages/internal-project/src/app.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ThemeSwitcher } from './components/theme-switcher';
 import { LinariaCard } from '@fedorovskyi/ui-kit';
+import { ThemeSwitcher } from './components/theme-switcher';
+import { InternalCard } from './components/internal-card';
 
 export const App = () => {
   return (
     <div>
       <ThemeSwitcher />
       <LinariaCard />
+      <InternalCard />
     </div>
   );
 };
---
+++ packages/internal-project/src/components/internal-card/internal-card.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const InternalCard = () => {
+  return (
+    <div>
+      <h2>Internal Card</h2>
+    </div>
+  )
+}
