3b6984385619d5e3494570c3c3ff65bc5e413ef9
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-25 12:34:53 +0200

    added nextjs-project

---
+++ packages/nextjs-project/app/page.tsx
@@ -0,0 +1,12 @@
+import {PigmentCard} from '@fedorovskyi/ui-kit';
+import {ThemeSwitcher} from '@/app/_components/theme-switcher';
+
+
+export default function Home() {
+    return (
+        <div>
+            <ThemeSwitcher/>
+            <PigmentCard/>
+        </div>
+    );
+}
